3dc2a4f8882ad169701b0be70b80b2df4df6850c
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-07-14 05:13:12 +0800

    adds tests for Cart.add_product method

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "when adding product to cart, set the line_item.product_price" do
+    cart = Cart.new
+    cart.add_product(products(:trakinas))
+
+    assert_equal products(:trakinas).price, cart.line_items.first.product_price
+  end
+
+  test "when adding product to a cart, update the line_item.product_price if it is already there" do
+    product = create(:product)
+    cart = Cart.new
+    cart.add_product(product).save!
+
+    assert_equal product.price, cart.line_items.first.product_price
+
+    product.price = 99.9
+    cart.add_product(product).save!
+
+    cart.reload
+
+    assert_equal product.price, cart.line_items.first.product_price
+  end
 end
