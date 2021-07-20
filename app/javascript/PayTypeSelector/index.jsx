36c8f16238e2eb9b44cd4b5d76768469bd53e95d
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-07-21 07:59:44 +0800

    dynamically load additional fields based on pay type

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react'
 
+import NoPayType from './NoPayType'
+import CheckPayType from './CheckPayType'
+import CreditCardPayType from './CreditCardPayType'
+import PurchaseOrderPayType from './PurchaseOrderPayType'
+
 export default function PayTypeSelector() {
   const [selectedPayType, setSelectedPayType] = useState(null)
 
@@ -7,15 +12,36 @@ export default function PayTypeSelector() {
     setSelectedPayType(event.target.value)
   }
 
+  let PayTypeComponent = NoPayType
+  switch (selectedPayType) {
+    case 'Check':
+      PayTypeComponent = CheckPayType
+      break;
+
+    case 'Credit card':
+      PayTypeComponent = CreditCardPayType
+      break;
+
+    case 'Purchase order':
+      PayTypeComponent = PurchaseOrderPayType
+      break;
+
+    default:
+      break;
+  }
+
   return (
-    <div className="field">
-      <label htmlFor="order_pay_type">Pay type</label>
-      <select id="order_pay_type" name="order[pay_type]" onChange={onPayTypeSelected}>
-        <option value="">Select a payment method</option>
-        <option valye="Check">Check</option>
-        <option valye="Credit card">Credit card</option>
-        <option valye="Purchase order">Purchase order</option>
-      </select>
+    <div>
+      <div className="field">
+        <label htmlFor="order_pay_type">Pay type</label>
+        <select id="order_pay_type" name="order[pay_type]" onChange={onPayTypeSelected}>
+          <option value="">Select a payment method</option>
+          <option valye="Check">Check</option>
+          <option valye="Credit card">Credit card</option>
+          <option valye="Purchase order">Purchase order</option>
+        </select>
+      </div>
+      <PayTypeComponent />
     </div>
   )
 }
