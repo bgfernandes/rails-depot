1bd43ffba4c0ae64705ccf938e4ca4fdf2e67863
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-07-26 04:50:37 +0800

    updates OrderMailer specs

--- spec/mailers/order_mailer_spec.rb
+++ spec/mailers/order_mailer_spec.rb
@@ -4,30 +4,32 @@ require 'rails_helper'
 
 RSpec.describe OrderMailer, :aggregate_failures, type: :mailer do
   describe 'received' do
-    let(:mail) { described_class.received }
+    let(:order) { create(:order) }
+    let(:mail) { described_class.received(order) }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Received')
-      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.subject).to eq('Cookie Store Order Confirmation')
+      expect(mail.to).to eq([order.email])
       expect(mail.from).to eq([Rails.application.credentials.email_settings[:email_from_address]])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to match(order.line_items.first.product.title)
     end
   end
 
   describe 'shipped' do
-    let(:mail) { described_class.shipped }
+    let(:order) { create(:order) }
+    let(:mail) { described_class.shipped(order) }
 
     it 'renders the headers' do
-      expect(mail.subject).to eq('Shipped')
-      expect(mail.to).to eq(['to@example.org'])
+      expect(mail.subject).to eq('Cookie Store Order Shipped')
+      expect(mail.to).to eq([order.email])
       expect(mail.from).to eq([Rails.application.credentials.email_settings[:email_from_address]])
     end
 
     it 'renders the body' do
-      expect(mail.body.encoded).to match('Hi')
+      expect(mail.body.encoded).to match(order.line_items.first.product.title)
     end
   end
 end
