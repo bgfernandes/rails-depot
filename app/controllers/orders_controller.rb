a2f1cc63aebfe40b61a038a0987a28cccde5317b
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-07-26 04:27:29 +0800

    Update orders_controller.rb
    send confirmation email on order completed

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,6 +32,7 @@ class OrdersController < ApplicationController
     respond_to do |format|
       if @order.save
         clear_cart
+        OrderMailer.received(@order).deliver_later
         format.html { redirect_to store_index_url, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
