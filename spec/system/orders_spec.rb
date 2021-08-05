dd0e02cb9e6adb17809dda51f1a831ca811f48c0
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-08-06 05:18:06 +0800

    adds system tests for checking if the order confirmation email is sent

--- spec/system/orders_spec.rb
+++ spec/system/orders_spec.rb
@@ -3,25 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe 'Orders', :aggregate_failures, type: :system do
+  include ActiveJob::TestHelper
+
   before do
     driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-    create(:order)
   end
 
-  it 'renders the index page' do
-    visit orders_path
-
-    expect(page).to have_selector('h1', text: 'Orders')
-  end
-
-  it 'destroys an order' do
-    visit orders_path
-
-    page.accept_confirm do
-      click_on 'Destroy', match: :first
+  context 'when there is already an existing order' do
+    before do
+      create(:order)
     end
 
-    expect(page).to have_text('Order was successfully destroyed.')
+    it 'renders the index page' do
+      visit orders_path
+
+      expect(page).to have_selector('h1', text: 'Orders')
+    end
+
+    it 'destroys an order' do
+      visit orders_path
+
+      page.accept_confirm do
+        click_on 'Destroy', match: :first
+      end
+
+      expect(page).to have_text('Order was successfully destroyed.')
+    end
   end
 
   context 'when creating a new order' do
@@ -33,6 +40,10 @@ RSpec.describe 'Orders', :aggregate_failures, type: :system do
       click_on 'Add to Cart', match: :first
 
       click_on 'Checkout'
+
+      fill_in 'Name', with: 'Customer Name'
+      fill_in 'Address', with: 'Customer Address'
+      fill_in 'Email', with: 'customer@email.com'
     end
 
     it 'does not show any pay_type specific fields' do
@@ -61,6 +72,36 @@ RSpec.describe 'Orders', :aggregate_failures, type: :system do
         expect(page).to have_no_selector('#order_expiration_date')
         expect(page).to have_no_selector('#order_po_number')
       end
+
+      context 'when filling up fields and submitting' do
+        before do
+          fill_in 'Routing #', with: '123456'
+          fill_in 'Account #', with: '987654'
+
+          perform_enqueued_jobs do
+            click_button 'Place Order'
+          end
+        end
+
+        let(:orders) { Order.all }
+        let(:order) { orders.first }
+        let(:email) { ActionMailer::Base.deliveries.last }
+
+        it 'creates the order' do
+          expect(orders.length).to be 1
+
+          expect(order.name).to eq 'Customer Name'
+          expect(order.address).to eq 'Customer Address'
+          expect(order.email).to eq 'customer@email.com'
+          expect(order.pay_type).to eq 'Check'
+        end
+
+        it 'sends the order confirmation email' do
+          expect(email.to).to eq ['customer@email.com']
+          expect(email[:from].value).to eq Rails.application.credentials.email_settings[:email_from]
+          expect(email.subject).to eq 'Cookie Store Order Confirmation'
+        end
+      end
     end
 
     context 'when selecting pay_type credit card' do
@@ -81,6 +122,36 @@ RSpec.describe 'Orders', :aggregate_failures, type: :system do
         expect(page).to have_no_selector('#order_account_number')
         expect(page).to have_no_selector('#order_po_number')
       end
+
+      context 'when filling up fields and submitting' do
+        before do
+          fill_in 'CC #', with: '123456'
+          fill_in 'Expiry', with: '03/19'
+
+          perform_enqueued_jobs do
+            click_button 'Place Order'
+          end
+        end
+
+        let(:orders) { Order.all }
+        let(:order) { orders.first }
+        let(:email) { ActionMailer::Base.deliveries.last }
+
+        it 'creates the order' do
+          expect(orders.length).to be 1
+
+          expect(order.name).to eq 'Customer Name'
+          expect(order.address).to eq 'Customer Address'
+          expect(order.email).to eq 'customer@email.com'
+          expect(order.pay_type).to eq 'Credit card'
+        end
+
+        it 'sends the order confirmation email' do
+          expect(email.to).to eq ['customer@email.com']
+          expect(email[:from].value).to eq Rails.application.credentials.email_settings[:email_from]
+          expect(email.subject).to eq 'Cookie Store Order Confirmation'
+        end
+      end
     end
 
     context 'when selecting pay_type purchase order' do
@@ -97,6 +168,35 @@ RSpec.describe 'Orders', :aggregate_failures, type: :system do
         expect(page).to have_no_selector('#order_account_number')
         expect(page).to have_no_selector('#order_credit_card_number')
         expect(page).to have_no_selector('#order_expiration_date')
+      end
+
+      context 'when filling up fields and submitting' do
+        before do
+          fill_in 'PO #', with: '123456'
+
+          perform_enqueued_jobs do
+            click_button 'Place Order'
+          end
+        end
+
+        let(:orders) { Order.all }
+        let(:order) { orders.first }
+        let(:email) { ActionMailer::Base.deliveries.last }
+
+        it 'creates the order' do
+          expect(orders.length).to be 1
+
+          expect(order.name).to eq 'Customer Name'
+          expect(order.address).to eq 'Customer Address'
+          expect(order.email).to eq 'customer@email.com'
+          expect(order.pay_type).to eq 'Purchase order'
+        end
+
+        it 'sends the order confirmation email' do
+          expect(email.to).to eq ['customer@email.com']
+          expect(email[:from].value).to eq Rails.application.credentials.email_settings[:email_from]
+          expect(email.subject).to eq 'Cookie Store Order Confirmation'
+        end
       end
     end
   end
