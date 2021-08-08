9e05dd465c8bbf4e3d638cfc57bbf8eadcb113ff
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-08-08 10:37:25 +0800

    scaffold users

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
+  factory :user do
+    name { "MyString" }
+    password { "" }
+  end
+
   factory :order do
     name { 'A name' }
     address { 'An address' }
