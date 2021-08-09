efd12a2c2f4b7869b94552ff91e52b6619e31fbe
Bruno Fernandes <bruno.gabriel.fernandes@gmail.com>
2021-08-10 02:41:51 +0800

    fixes/adds specs for login and logout

--- spec/requests/admin_spec.rb
+++ spec/requests/admin_spec.rb
@@ -3,9 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Admins', type: :request do
-  describe 'GET /index' do
+  describe 'GET /admin' do
     it 'returns http success' do
-      get '/admin/index'
+      get '/admin'
       expect(response).to have_http_status(:success)
     end
   end
